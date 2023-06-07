4e65f45bfb031931558bafdfecfa98d14f945f12
Bianca <134503670+BiancaScanfella@users.noreply.github.com>
2023-06-07 14:24:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula06CursoSerasaGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! esse é o meu projeto");
+            Console.WriteLine("Ele é um teste para o GitHub");
+            Console.WriteLine("Nada de novo por aqui...");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
